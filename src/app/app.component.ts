53faf30b656b9eaf13d348e8b285200adce03303
matias chavez <matroxmega@gmail.com>
2019-05-09 16:53:56 -0400

    IDK that i push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IWord } from './IWord';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcadoMaty';
+  title = 'Juego del Ahorcado';
+  acciones = 'Acciones';
+  estadisticas ='Estadisticas';
+  aciertos='Aciertos:';
+  desaciertos='Desaciertos:';
+  totalJugadas='Total de Jugadas:';
+  sms='Mensaje';
+  student='Estudiante: ';
+  nameclass='- Desarrollo Frontend con Angular';
+  Words: IWord[];
+
+
+
+  //cosas x cambiar
+  numaciertos= '';
+  numdesaciertos= '';
+  numtotaljugadas= '';
+  guiones='guiones qlos chupen el perro';
+  nomestudiante='Matias Chavez Saavedra';
+
+
 }
